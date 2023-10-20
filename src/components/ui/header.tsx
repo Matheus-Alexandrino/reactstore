07d65e1267eec0641130f2e957e1cc3414b464fb
Matheus-Alexandrino <matwsalmeida@gmail.com>
2023-10-20 01:40:48 -0300

    feat: create buttons of header

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,19 +1,31 @@
-import { MenuIcon, ShoppingCartIcon } from "lucide-react";
+import { MenuIcon, Sheet, ShoppingCartIcon } from "lucide-react";
 import { Button } from "./button";
-import { Card, CardContent } from "./card"
+import { Card } from "./card"
+import { SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 
 const Header = () => {
     return <Card className="flex items-center justify-between p-[1.875rem] ">                
-                <Button size="icon" variant="outline">
-                    <MenuIcon/>
-                </Button>  
+                <Sheet>
+                    <SheetTrigger asChild>
+                        <Button size="icon" variant="outline">
+                            <MenuIcon/>
+                        </Button>  
+                    </SheetTrigger>
                 
+                    <SheetContent side="left">
+                            <SheetHeader className="text-left text-lg font-semibold">
+                                Menu
+                            </SheetHeader>
+                    </SheetContent>
+                </Sheet> 
                 <h1 className="text-lg font-semibold">
-                    <span className="text-primary">React</span>Store</h1>
-                <Button size="icon" variant="outline">
-                    <ShoppingCartIcon />
+                    <span className="text-primary">React</span>Store
+                </h1>
+
+                    <Button size="icon" variant="outline">
+                        <ShoppingCartIcon />
                 </Button>              
-           </Card>;
+            </Card>;
 }
  
 export default Header;
