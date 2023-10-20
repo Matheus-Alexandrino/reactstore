1170539d8912eb563d232c8988dad8164cd8376f
Matheus-Alexandrino <matwsalmeida@gmail.com>
2023-10-20 04:21:53 -0300

    feat: including next-auth and configuration google auth keys

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+import { prismaclient } from "@/lib/prisma"
+
+// const prisma = new PrismaClient()
+
+export default NextAuth({
+  adapter: PrismaAdapter(prismaClient),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    })
+  ]
+})
---
+++ src/lib/prisma.ts
@@ -0,0 +1,19 @@
+//quando esta usando Next Js13 com prisma precisa tomar cuidado em sempre que reiniciar o servidor
+//irá criar uma conexão nova a cada reload
+// define apenas um prisma client
+import {PrismaClient} from "@prisma/client";
+
+declare global {
+    var cachedPrisma: PrismaClient;
+}
+
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+    prisma = new PrismaClient();
+} else {
+    if (!global.cachedPrisma){
+        global.cachedPrisma = new PrismaClient();
+    }
+    prisma = global.cachedPrisma;
+}
+export const prismaclient = prisma;
