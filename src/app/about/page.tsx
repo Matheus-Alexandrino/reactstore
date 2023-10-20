a5021566eabedc184c5a21afa61eb9a576bf90c3
Matheus-Alexandrino <matwsalmeida@gmail.com>
2023-10-19 23:18:00 -0300

    chore: install prettier.config.js

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import image from "next/image";
+
+export default function Home() {
+    return (
+        <div className="text-red-500 bg-blue-500">
+
+        Hello Cabrito
+        </div>
+    );
+}
