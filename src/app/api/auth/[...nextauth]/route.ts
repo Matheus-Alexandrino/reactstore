9eaf59c9da270d237f6f65362754500f999f8109
Matheus-Alexandrino <matwsalmeida@gmail.com>
2023-10-21 21:53:00 -0300

    feat: Add provider google and next- auth -02

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth"
-// import GoogleProvider from "next-auth/providers/google"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
+import { authOptions } from "@/lib/auth"
 
-const prisma = new PrismaClient()
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST};
 
 
