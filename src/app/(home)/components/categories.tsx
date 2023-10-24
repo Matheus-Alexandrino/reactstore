daeb7a4232c24ac531dd172829ee82227a4bcbf8
Matheus-Alexandrino <matwsalmeida@gmail.com>
2023-10-24 00:59:24 -0300

    feat: add badges of category and others features of ui

---
+++ src/app/(home)/components/categories.tsx
@@ -0,0 +1,18 @@
+
+import { prismaclient } from "@/lib/prisma";
+import CategoryItem from "./category-item";
+
+const Categories = async () => {
+    const categories = await prismaclient.category.findMany({})
+
+    return ( 
+        <div className="grid grid-cols-2  gap-y-2 gap-x-4">
+            {categories.map((category) => (
+            <CategoryItem key={category.id} category={category}/>
+            ))}
+
+        </div>
+     );
+}
+ 
+export default Categories;
