29a94650dbc187b7a9d14385bd74fe5fd6ba5856
Matheus-Alexandrino <matwsalmeida@gmail.com>
2023-10-24 09:46:02 -0300

    fix: problem with auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,16 +3,5 @@ import GoogleProvider from "next-auth/providers/google";
 import { AuthOptions } from "next-auth";
 import { prismaclient } from "./prisma";
 
-export const authOptions: AuthOptions = {
-   
-  adapter: PrismaAdapter(PrismaClient),
-  providers: [
-    GoogleProvider({
-  
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      
-    }),
-  ],
-}
+
 
