9721545cc122ffc2bd2ae84487aa55e1a3e2d2d0
Matheus-Alexandrino <matwsalmeida@gmail.com>
2023-10-24 01:06:37 -0300

    test: deploy not included .env

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,16 +3,16 @@ import GoogleProvider from "next-auth/providers/google";
 import { AuthOptions } from "next-auth";
 import { prismaclient } from "./prisma";
 
-export const authOptions: AuthOptions = {
+// export const authOptions: AuthOptions = {
    
-  adapter: PrismaAdapter(prismaclient),
-  providers: [
-    GoogleProvider({
+//   adapter: PrismaAdapter(prismaclient),
+//   providers: [
+//     GoogleProvider({
   
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//         clientId: process.env.GOOGLE_CLIENT_ID,
+//         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       
-    }),
-  ],
-}
+//     }),
+//   ],
+// }
 
