59ac74b47bbe8d49faf2144303ef9a6369c87473
Matheus-Alexandrino <matwsalmeida@gmail.com>
2023-10-24 09:49:04 -0300

    fix: problem with auth.ts 002

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,2 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import GoogleProvider from "next-auth/providers/google";
-import { AuthOptions } from "next-auth";
-import { prismaclient } from "./prisma";
-
 
 
