3d41ddfe76f1a18ca01d6fe470dd182127b73a6e
Matheus-Alexandrino <matwsalmeida@gmail.com>
2023-10-24 08:32:58 -0300

    fix: problems with deploy

--- src/providers/auth.tsx
+++ src/providers/auth.tsx
@@ -2,7 +2,7 @@
 
 import { SessionProvider, SessionProviderProps } from "next-auth/react";
 
-// import {SessionProvider, SessionProviderProps} from "next-auth/react";
+
 export const AuthProvider= ({ children }: SessionProviderProps) => {
     return <SessionProvider>{children}</SessionProvider>;
 };
